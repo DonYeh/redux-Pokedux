9537cfed24b1bf033f70775312edd36d7b2e7580
Donald Yeh <Donald.Yeh@gmail.com>
2019-05-07 09:49:52 -0400

    adds skeleton of redux code with pokemon deck

--- src/index.js
+++ src/index.js
@@ -1,10 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ======================================================================
+
+// STATE
+// ======================================================================
+
+// STORE
+
+// ======================================================================
+// STATE
+import initialState from "./base.json";
+console.log(initialState);
+
+// the state is an objet with a cards property
+// which is an array of objects
+// { cards: [ {}. {}, {} ]}
+
+// ======================================================================
+// ACTIONS + ACTION CREATORS
+const ACTION_CATCH = "catch";
+
+function catchCard(id) {
+	return {
+		type: ACTION_CATCH,
+		payload: {
+			id
+		}
+	};
+}
+
+// ======================================================================
+// REDUCER
+function cards(state = initialState, action = { type: "" }) {
+	switch (action.type) {
+		case ACTION_CATCH:
+			// find the card, set it to "caught"
+			break;
+		default:
+			return state;
+			break;
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
